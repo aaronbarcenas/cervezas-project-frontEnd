ed3be87a1502e5596f0d4ccb6e83ce4a3553204e
MarioAndrade <yomariolalo@hotmail.com>
2021-04-09 18:14:20 -0500

    Anexo ejercicio

--- js/main.js
+++ js/main.js
@@ -1,218 +1,85 @@
-const getBeer = () => {
+const getBeerInfo = () => {
   let beerObject = {}
   let fields = document.querySelectorAll("form input[type='text']");
 
-  fields.forEach( field => {
-    beerObject[field.name] = field.value
-    console.log(beerObject)
+  fields.forEach(field => {
+      beerObject[field.name] = field.value
+      console.log(beerObject)
   })
-
   let select = document.getElementById("category")
   let category = select.options[select.selectedIndex].value
 
   console.log(category)
-
-  beerObject = {
-    ...beerObject,
-    category
-  }
-
+  beerObject = { ...beerObject, category }
   console.log(beerObject)
 
-  saveBeer( beerObject )
-
-  fields.forEach( field => {
-    field.value = ""
+  saveBeer(beerObject)
+  fields.forEach(field => {
+      field.value = ""
   })
 }
 
-//$('#save-succesful').modal("show")
 
-document.getElementById("save-Beer").addEventListener("click", getBeer)
+document.getElementById("save-Beer").addEventListener("click", getBeerInfo)
 
 const saveBeer = beer => {
-  let xhttp = new XMLHttpRequest(); /**/ 
-  
-  xhttp.onreadystatechange = function() {
+  let xhttp = new XMLHttpRequest(); /**/
+
+  xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-         console.log( xhttp.response )
-         $('#save-succesful').modal('show')
-          printTable( getBeersCollection() )
+          console.log(xhttp.response)
+          $('#save-succesful').modal('show')
+          printTable(getBeersCollection())
       }
   }
-
   xhttp.open("POST", "https://ajaxclass-1ca34.firebaseio.com/11g/israel/beers.json", true);
-
-  xhttp.send( JSON.stringify(beer) );
+  xhttp.send(JSON.stringify(beer));
 }
 
 const getBeersCollection = () => {
   let beersCollection;
   let xhttp = new XMLHttpRequest();
-  
-  xhttp.onreadystatechange = function() {
+
+  xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-          console.log( xhttp.response )
-          beersCollection = JSON.parse( xhttp.response)
+          console.log(xhttp.response)
+          beersCollection = JSON.parse(xhttp.response)
       }
   }
-
-  xhttp.open("GET", "https://ajaxclass-1ca34.firebaseio.com/11g/israel/beers.json", false );
-
+  xhttp.open("GET", "https://ajaxclass-1ca34.firebaseio.com/11g/israel/beers.json", false);
   xhttp.send();
-
-  return beersCollection    
+  return beersCollection
 }
 
-const deleteBeer = event => { /*recibimos como parámetro el botón al que se le hace click*/
-  console.log( event.target )
-  /*extraemos la llave del disco*/
+const deleteBeer = event => {
+  console.log(event.target)
   let beerKey = event.target.dataset.beerKey
-  console.log( beerKey )
-
-  /*lanzamos la petición para borrar el disco*/
+  console.log(beerKey)
   let xhttp = new XMLHttpRequest();
-  
-  xhttp.onreadystatechange = function() {
+
+  xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-          console.log( xhttp.response )
-          printTable( getBeersCollection() )
+          console.log(xhttp.response)
+          printTable(getBeersCollection())
       }
   }
-
-  xhttp.open("DELETE", `https://ajaxclass-1ca34.firebaseio.com/11g/israel/beers/${beerKey}/.json`, false );
-
+  xhttp.open("DELETE", `https://ajaxclass-1ca34.firebaseio.com/11g/israel/beers/${beerKey}/.json`, false);
   xhttp.send();
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const printTable =  dataToPrint => {
-  console.log( dataToPrint )
+const printTable = dataToPrint => {
+  console.log(dataToPrint)
   let table = document.getElementById("beer-table")
   let index = 1
 
-  /*limpiamos la tabla antes de imprimirla*/    
-  while (table.lastElementChild) {
-      table.removeChild( table.lastElementChild );
-  }
 
-  /*iteramos en la lista de discos y creamos los elementos correspondientes de cada fila*/
-  for( key in dataToPrint ){
-      console.log( key )
-      console.log( dataToPrint[key] )
+  while (table.lastElementChild) {
+      table.removeChild(table.lastElementChild);
+  }
+  for (key in dataToPrint) {
+      console.log(key)
+      console.log(dataToPrint[key])
 
       let { name, description, price, image, category } = dataToPrint[key]
 
@@ -226,36 +93,34 @@ const printTable =  dataToPrint => {
       let categoryTd = document.createElement("td")
       let buttonTd = document.createElement("td")
 
-      descriptionTd.classList = "text-truncate"
-      descriptionTd.style.maxWidth = "100px"
+      // descriptionTd.classList = "text-truncate"
+      // descriptionTd.style.maxWidth = "100px"
 
-      imageTd.classList = "text-truncate"
-      imageTd.style.maxWidth = "100px"
+      // imageTd.classList = "text-truncate"
+      // imageTd.style.maxWidth = "100px"
 
-      let indexText = document.createTextNode( index )
-      let nameText = document.createTextNode( name )
-      let descriptionText = document.createTextNode( description )
-      let priceText = document.createTextNode( price )
-      let imageText = document.createTextNode( image )
-      let categoryText = document.createTextNode( category )
-
-      
+      let indexText = document.createTextNode(index)
+      let nameText = document.createTextNode(name)
+      let descriptionText = document.createTextNode(description)
+      let priceText = document.createTextNode(price)
+      let imageText = document.createTextNode(image)
+      let categoryText = document.createTextNode(category)
 
       let deleteButton = document.createElement("button")
       deleteButton.classList = "btn btn-outline-danger delete-button mt-1"
-      deleteButton.dataset.beerKey = key 
+      deleteButton.dataset.beerKey = key
 
       let buttonText = document.createTextNode("Borrar")
 
       deleteButton.appendChild(buttonText)
 
-      indexTd.appendChild( indexText )
-      nameTd.appendChild( nameText )
-      descriptionTd.appendChild( descriptionText )
-      priceTd.appendChild( priceText )
-      imageTd.appendChild( imageText )
-      categoryTd.appendChild( categoryText )
-      buttonTd.appendChild( deleteButton )
+      indexTd.appendChild(indexText)
+      nameTd.appendChild(nameText)
+      descriptionTd.appendChild(descriptionText)
+      priceTd.appendChild(priceText)
+      imageTd.appendChild(imageText)
+      categoryTd.appendChild(categoryText)
+      buttonTd.appendChild(deleteButton)
 
       beerRow.appendChild(indexTd)
       beerRow.appendChild(nameTd)
@@ -263,19 +128,20 @@ const printTable =  dataToPrint => {
       beerRow.appendChild(priceTd)
       beerRow.appendChild(imageTd)
       beerRow.appendChild(categoryTd)
-      beerRow.appendChild(deleteButton)
+      beerRow.appendChild(buttonTd)
 
       table.appendChild(beerRow)
       index++
-  }
 
+  }
   /*agrupamos todos los botones*/
   let buttons = document.querySelectorAll(".delete-button")
 
   /*agregamos el listener a cada botón*/
-  buttons.forEach( button => {
-      button.addEventListener("click", deleteBeer )
+  buttons.forEach(button => {
+      button.addEventListener("click", deleteBeer)
   })
-} 
+}
 
-printTable( getBeersCollection() )
+printTable(getBeersCollection())
+
